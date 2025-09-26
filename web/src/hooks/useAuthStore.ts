bbd931dfff5428b320e643823b1801863f16ddd7
Ausath Ikram <ausathdzil@gmail.com>
2025-09-26 10:33:51 +0700

    Remove isAuth flag, add react-scan

--- web/src/hooks/useAuthStore.ts
+++ web/src/hooks/useAuthStore.ts
@@ -3,12 +3,9 @@ import { create } from 'zustand';
 type AuthState = {
   accessToken: string | null;
   setAccessToken: (token: string | null) => void;
-  isAuth: boolean;
 };
 
 export const useAuthStore = create<AuthState>()((set) => ({
   accessToken: null,
-  setAccessToken: (token) =>
-    set({ accessToken: token, isAuth: token !== null }),
-  isAuth: false,
+  setAccessToken: (token) => set({ accessToken: token }),
 }));
